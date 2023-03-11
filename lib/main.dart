5081c9062670c94462b805df241417f7f9958270
nxj1129 <niko.jugovac@gmail.com>
2023-03-11 15:12:27 +0100

    Added navigation for register and login views

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:testproject/firebase_options.dart';
 import 'package:testproject/views/login_view.dart';
+import 'package:testproject/views/register_view.dart';
 
 String getFullName(String firstName, String lastName) {
   return firstName + lastName;
@@ -16,6 +17,10 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: const HomePage(),
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView(),
+    },
   ));
 }
 
@@ -24,24 +29,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              print(FirebaseAuth.instance.currentUser);
-              return const Text('Done');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            //   final user = FirebaseAuth.instance.currentUser;
+            //   if (user?.emailVerified ?? false) {
+            //     return const Text('Done');
+            //   } else {
+            //     return const VerifyEmailView();
+            //   }
+
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
