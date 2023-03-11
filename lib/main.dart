fcdc2b0d257415da0d4d31e1f0dd7caf9ed6b4d4
nxj1129 <niko.jugovac@gmail.com>
2023-03-11 15:19:29 +0100

    Change views depending on user email

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:testproject/firebase_options.dart';
 import 'package:testproject/views/login_view.dart';
 import 'package:testproject/views/register_view.dart';
 
+import 'views/verify_email_view.dart';
+
 String getFullName(String firstName, String lastName) {
   return firstName + lastName;
 }
@@ -36,14 +38,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            //   final user = FirebaseAuth.instance.currentUser;
-            //   if (user?.emailVerified ?? false) {
-            //     return const Text('Done');
-            //   } else {
-            //     return const VerifyEmailView();
-            //   }
-
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
